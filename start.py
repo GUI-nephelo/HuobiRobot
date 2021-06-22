8ff8944c699aceef10e60a9eb465ec3500d04d3a
Garin0828 <awGZbyXZEeJNnufCVKFoQg@hotmail.com>
2021-06-23 00:22:14 +0800

    test

--- start.py
+++ start.py
@@ -3,25 +3,38 @@ import os,json
 import mainRobot
 import socket,socks
 
-#socks.set_default_proxy(socks.SOCKS5,"127.0.0.1",10808)
+#socks.set_default_proxy(socks.PROXY_TYPE_HTTP,"127.0.0.1",10809)
 #socket.socket = socks.socket
 
-n=1
+#import ssl
+#ssl._create_default_https_context = ssl._create_unverified_context
 
 with open("./keys.json","r") as f:
     a=json.loads(f.read())
     (api_key, secret_key) = a[n]["accessKey"],a[n]["secretKey"]
 
 if __name__=="__main__":
+    """
+    from huobi.client.market import MarketClient
+    from huobi.constant import *
+    from huobi.utils import *
+
+    market_client = MarketClient(init_log=True)
+    interval = CandlestickInterval.MIN1
+    symbol = "btc3lusdt"
+    list_obj = market_client.get_candlestick(symbol, interval, 10)
+    LogInfo.output("---- {interval} candlestick for {symbol} ----".format(interval=interval, symbol=symbol))
+    LogInfo.output_list(list_obj)
+    """
     from huobi.client.account import AccountClient
     from huobi.constant import *
-
-    # get accounts
-    from huobi.utils import *
+    from huobi.utils import LogInfo
 
     account_client = AccountClient(api_key=api_key,
                                    secret_key=secret_key)
-
-    list_obj = account_client.get_accounts()
-    print(list_obj)
-    #LogInfo.output_list(list_obj)
+    LogInfo.output("====== (SDK encapsulated api) not recommend for low performance and frequence limitation ======")
+    account_balance_list = account_client.get_account_balance()
+    if account_balance_list and len(account_balance_list):
+        for account_obj in account_balance_list:
+            account_obj.print_object()
+            print()
